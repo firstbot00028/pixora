3bad83e2e8a02247c82cc09ff67be633a46130bf
firstbot00028 <m02661089@gmail.com>
2026-03-11 12:38:51 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,63 @@
+// LOGIN FUNCTION
+
+function login(){
+
+let username=document.getElementById("username").value
+let password=document.getElementById("password").value
+
+if(username==="AiraAdam" && password==="Aira123"){
+
+alert("Welcome Founder")
+
+window.location="home.html"
+
+}else{
+
+alert("Invalid login")
+
+}
+
+}
+
+
+// SIMPLE MESSAGE SYSTEM
+
+function send(){
+
+let msg=document.getElementById("msg").value
+
+if(msg==="") return
+
+let chat=document.getElementById("chat")
+
+let div=document.createElement("div")
+
+div.innerText=msg
+
+chat.appendChild(div)
+
+document.getElementById("msg").value=""
+
+}
+
+
+// SIMPLE POST CREATE (frontend demo)
+
+function createPost(){
+
+let caption=document.getElementById("caption").value
+
+let feed=document.getElementById("feed")
+
+let post=document.createElement("div")
+
+post.className="post"
+
+post.innerHTML=`
+<h4>@AiraAdam <span class="redtick">✔</span></h4>
+<p>${caption}</p>
+`
+
+feed.prepend(post)
+
+}
